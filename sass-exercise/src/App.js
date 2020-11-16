1d4c1de9a89f5363e2408f6881ad96b3abc2bbd7
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-11-16 14:17:24 +0000

    rough draft complete

--- sass-exercise/src/App.js
+++ sass-exercise/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './main.scss';
+// import './main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div id="landscape">
+        <h1 className="title">Visit Myanmar</h1>
+      </div>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Descriptions to come. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consectetur, libero eget bibendum elementum, lorem orci rutrum justo, at fermentum neque felis ac ex. Phasellus suscipit dapibus ante, eu sodales risus sagittis ut. Phasellus eget augue purus. Sed eget auctor felis, egestas vestibulum augue. Aenean pharetra metus quis orci consequat dapibus. Integer aliquam aliquet ipsum ut pellentesque. Curabitur cursus, nibh ut vehicula maximus, eros libero pretium diam, eu luctus nisl mauris a augue. Nulla luctus in orci non vulputate. Suspendisse at tortor elit.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div id="monkeys">
+         <h1 className="title">Book my trip now</h1>
+       </div>
+
     </div>
   );
 }
